b7ea75a4f3a85446b74782ea83fbcedc599f60f3
Geovanny Briceño <gebri106126@gmail.com>
2023-09-01 16:24:44 -0600

    aprendiendo que son, que pasan y como se pasan las props

--- proyects/00-hola-mundo/src/App.jsx
+++ proyects/00-hola-mundo/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import { TwitterFollowCard } from './twitterFollowCard'
 export function App () {
-    
+const formatUserName = (userName) => `@${userName}`
 return(
     <section className='app'>
-    <TwitterFollowCard isfollowing username="@luisgod" name="Luis Amaya"/>
-    <TwitterFollowCard isfollowing={false} username="@Kenethdevil" name="Keneth Sanchez"/>
-    <TwitterFollowCard isfollowing username="@rafaonlyone" name="Rafael Martinez"/>
-    <TwitterFollowCard isfollowing username="@navarroeldestructor" name="Olger Navarro"/>
+    <TwitterFollowCard isfollowing userName="luisgod" >
+    Luis Amaya
+    </TwitterFollowCard>
+    <TwitterFollowCard isfollowing={false} userName="Kenethdevil">
+     Keneth Sanchez
+     </TwitterFollowCard>
+    <TwitterFollowCard isfollowing userName="rafaonlyone">
+   Rafael Martinez
+   </TwitterFollowCard>
+ 
     </section>
      
 )
--- proyects/00-hola-mundo/src/twitterFollowCard.jsx
+++ proyects/00-hola-mundo/src/twitterFollowCard.jsx
@@ -1,18 +1,16 @@
 
-export function TwitterFollowCard ({username,name,isfollowing}){
+export function TwitterFollowCard ({children,userName,isfollowing}){
    //primera forma de pasar la info a el Src de la Img
   //const imgSrc = `https://unavatar.io/${username}`
     return(
         <article className='tw-followCard'>
         <header className='tw-followCard-header'>
-            <img src={`https://unavatar.io/${username}`}
+            <img src={`https://unavatar.io/${userName}`}
             alt="el avatar de luis" 
             className='tw-followCard-img' />
             <div className='tw-followCard-div'>
-                <stron>
-                  {name}
-                </stron>
-                <span className='tw-followCard-header-span'>{username}</span>
+              <stron> {children} </stron>
+                <span className='tw-followCard-header-span'>@{userName}</span>
             </div>
         </header>
         <aside>
